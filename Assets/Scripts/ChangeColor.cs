8aaeab6261fae97cc0a6ae4141293d2f471ce7f9
Humberto Dias <hldtux@gmail.com>
2019-09-15 01:21:22 +0200

    Adding complete tutorial

---
+++ Assets/Scripts/ChangeColor.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+// https://medium.com/@kleberandrade/trabalhando-com-joints-na-unity-9eaacc7632c6
+public class ChangeColor : MonoBehaviour
+{
+    public Color m_MinColor = Color.white;
+    public Color m_MaxColor = Color.white;
+    private Renderer m_Renderer;
+
+    private void Start()
+    {
+        m_Renderer = GetComponent<Renderer>();
+        Color color = Color.Lerp(m_MinColor, m_MaxColor, Random.Range(0.0f, 1.0f));
+        m_Renderer.material.SetColor("_Color", color);
+    }
+}
--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# unity-joints
+# Unity Joints
+
+Following the tutorial https://medium.com/@kleberandrade/trabalhando-com-joints-na-unity-9eaacc7632c6
+Tested on Linux using Unity version 2019.3.0b3
+
+![](demo.gif)
