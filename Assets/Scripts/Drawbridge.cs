3031fe9102284aae95606a183432a5cc62603d99
Humberto Dias <hldtux@gmail.com>
2022-09-28 22:45:05 +0200

    Selectable bridge

--- Assets/Scripts/Drawbridge.cs
+++ Assets/Scripts/Drawbridge.cs
@@ -6,19 +6,35 @@ public class Drawbridge : MonoBehaviour
     private int m_Index = 0;
     private HingeJoint m_Joint;
 
+    private Renderer m_renderer;
+
     private void Awake()
     {
         m_Joint = GetComponent<HingeJoint>();
+        m_renderer = GetComponent<Renderer>();
     }
 
-    private void Update()
+    private void Draw()
     {
-        if (Input.GetButtonDown("Fire1"))
-        {
-            JointSpring spring = m_Joint.spring;
-            m_Index = ++m_Index % m_Targets.Length;
-            spring.targetPosition = m_Targets[m_Index];
-            m_Joint.spring = spring;
-        }
+        JointSpring spring = m_Joint.spring;
+        m_Index = ++m_Index % m_Targets.Length;
+        spring.targetPosition = m_Targets[m_Index];
+        m_Joint.spring = spring;
     }
+
+    private void OnMouseEnter()
+    {
+        m_renderer.material.color = Color.red;
+    }
+
+    private void OnMouseExit()
+    {
+        m_renderer.material.color = Color.white;
+    }
+
+    private void OnMouseUp()
+    {
+        Draw();
+    }
+
 }
